300b105bb8557e83bb45424a846adc689085cfaf
erglobant <54032346+erglobant@users.noreply.github.com>
2019-09-05 17:54:51 -0500

    html semantic and js improvements
    The recommendations done by Gabriel were done

--- cope-august-2019/calculator/esteban-restrepo-ramirez/calculator.js
+++ cope-august-2019/calculator/esteban-restrepo-ramirez/calculator.js
@@ -1,105 +1,138 @@
 
 //DOM elements
-var digitClass 		= document.getElementsByClassName("digit");
-var operatorClass 	= document.getElementsByClassName("operator");
-var equalButton 	= document.getElementById("equal");
-var cleanButton 	= document.getElementById("clean");
+const display 	= document.getElementById("display");
+const digitTypeElements 	= document.getElementsByClassName("operand");
+const operatorTypeElements  = document.getElementsByClassName("operator");
+const equalButton = document.getElementById("equal");
+const cleanButton = document.getElementById("clean");
 
-var digits 		= [];
-var operators 	= [];
-var lastButtonTyped = 0; //tis variable is the flag to know if the previous button pressed was a digit (0) or and operator (1)
-var math_it_up = {
-    "+": function (x, y) { return x + y },
-    "-": function (x, y) { return x - y },
-	"*": function (x, y) { return x * y },
-	"/": function (x, y) { return x / y }
+const nonNumsOrOperators =/[^0-9\*\+\-\/]/gi;
+
+let operands 		 = [];
+let operatorsPressed = [];
+let displayValueBeforeEnterNewDigit;
+const isTheDigitanOperator = (digit) => operate.hasOwnProperty(digit);
+const isCurrentEventAClick = (eventType) => eventType == 'click';
+
+const setValueFromKeyboard = (event) => {
+	let keyPressed = event.key;
+	
+	if(/[0-9]/g.test(keyPressed)){
+		setOperand(event);
+	}else if(isTheDigitanOperator(keyPressed)){
+		setOperator(event);
+	}else if(/(=|Enter)/.test(keyPressed)){
+		calculateResult();
+	}else if(keyPressed == 'Backspace' &&  !display.value){
+		display.value = '0';
+	}else{
+		display.value = display.value.replace(nonNumsOrOperators, '');
+	}
+}
+
+const setOperand = (event) => {
+	const operand = event.key || event.target.textContent;
+	
+	if(!isTheDigitanOperator(displayValueBeforeEnterNewDigit.slice(-1)) && operands.length > 0){
+		const indexfromLastOperandAdded = operands.length-1; 
+		operands[indexfromLastOperandAdded] = parseInt(operands[indexfromLastOperandAdded]+operand);
+	}else{
+		operands = operands.concat([parseInt(operand)]);
+	}
+	
+	if(isCurrentEventAClick(event.type)) display.value += operand;
 };
 
-//digits click event
-Array.from(digitClass).forEach(function(element) {
-	element.addEventListener('click', function(){
-		var digit = this.textContent;
-		
-		//if the default value is there, lets clean the screen 
-		if(document.getElementById('result').value == 0) document.getElementById('result').value = '';
-		
-		//set the digit pressed into the screen
-		document.getElementById('result').value += digit;
-		
-		//if the last element pressed was a digit
-		if(lastButtonTyped == 0 && digits.length >0 ){
-			var lastElement = digits.length-1; 
-			digits[lastElement] += digit;
-			digits[lastElement] = parseInt(digits[lastElement])
-		}else{ //it means the last element pressed was an operator
-			digits.push(parseInt(digit));
-		}
-		lastButtonTyped = 0;
-	});
-});
+const setOperator = (event) => {
+	const operator = event.key || event.target.textContent;
+	
+	if(operator == '=' 
+		|| operator == 'CE' 
+		|| isTheDigitanOperator(displayValueBeforeEnterNewDigit.slice(-1))) return;
+	
+	/*for those cases where the user for some reason just enter one operand, 
+	we need to operate it with the 0 that we have there as default value*/
+	if(operands.length == 0) operands = [0];
+	
+	operatorsPressed = operatorsPressed.concat([operator]);
+	if(isCurrentEventAClick(event.type)) display.value += operator;
+};
 
-//operators click event
-Array.from(operatorClass).forEach(function(element) {
-	element.addEventListener('click', function(){
-		var operator = this.textContent;
-		
-		//those guys are belong to the operator css class, but dont need to be included in this event logic
-		if(operator == "=" || operator == "CE" || lastButtonTyped == 1) return;
-		
-		/*for those cases where the user for some reason just enter one digit, 
-		we need to operate it with the 0 that we have there as default value*/
-		if(digits.length == 0){
-			digits[0] = 0;
-		}
-		operators.push(operator);
-		document.getElementById('result').value += operator;
-		
-		lastButtonTyped = 1;
-	});
-});
-
-//result click event
-equalButton.addEventListener('click', function(){
-	var result = 0;
+const removeElementFromDisplay = (event) => {
+	let displayValue = display.value ;
 	
+	let deletedValueFromDisplay = displayValue.slice(-1);
 	
-	if(operators.length == 0) return;
-	
-	operators.forEach(function(op){
-		
-		//if isn't the first iteration
-		if(result != 0){
-			result = math_it_up[op](result, digits[0]);
-			digits.shift();
-		}else{ //otherwise
-			if(digits.length < 2) return;
-			
-			//te function called here will run the right operation according the user input
-			result = math_it_up[op](digits[0], digits[1]);
-			digits.splice(0,2);
-		}
-	});
-	
-	digits		= [];
-	operators 	= [];
-	document.getElementById('result').value = result;
-	digits[0]= parseInt(document.getElementById('result').value);
-});
-
-//clean screen click event
-cleanButton.addEventListener('click', function(){
-	var currentValue = document.getElementById('result').value;
-	
-	//if there is nothing in these arrays we dont want to delete the defaul 0 from the screen
-	if(operators.length == 0 && digits.length == 0) return;
-	
-	//if the char to delete from screen is a number
-	if(!isNaN(currentValue[currentValue.length-1])){
-		if(digits[digits.length-1].toString().length > 1) digits[digits.length-1] = parseInt(digits[digits.length-1].toString().slice(0, -1));
-		else digits.pop();
+	if(isCurrentEventAClick(event.type)) {
+		displayValue = displayValue.slice(0,-1);
+		display.value = displayValue.length > 0 ? displayValue : '0';
 	}
-	else operators.pop();
 	
-	var subString = currentValue.slice(0, -1);
-	document.getElementById('result').value = subString.length != 0 ? subString : "0";
+	if(isTheDigitanOperator(deletedValueFromDisplay)){
+		operatorsPressed = operatorsPressed.slice(0, -1);
+	}else if(operands.length > 0){
+		const operandsQty = operands.length;
+		let lastOperand = operands[operandsQty-1].toString();
+		if(lastOperand.length > 1){
+			operands[operands.length-1] = parseInt(lastOperand.slice(0, -1));
+		}
+		else operands = operands.slice(0, -1);
+	}
+};
+
+const operate = {
+    "+": (x, y) => x + y,
+    "-": (x, y) => x - y,
+	"*": (x, y) => x * y,
+	"/": (x, y) => x / y
+};
+
+const calculateResult = () =>{
+	let operationResult = 0;
+	
+	if(operatorsPressed.length == 0) return;
+	
+	let [firstOperand, ...operandsWithoutFirstElm] = operands;
+	let operatorsPressedClone = [...operatorsPressed];
+	
+	operationResult = operandsWithoutFirstElm.reduce((acc, operand) => {
+		const [currentOperator] = operatorsPressedClone; 
+		operatorsPressedClone = operatorsPressedClone.slice(1);
+		return operate[currentOperator](acc, operand);
+	}, firstOperand);
+	
+	operatorsPressed = [];
+	operands = [parseInt(operationResult)];
+	display.value = operationResult;
+}
+
+Array.from(digitTypeElements).forEach((element) => {
+	element.addEventListener('click', (event) => {
+		//if the default value is there, lets clean the display 
+		if(display.value == 0) 
+			display.value = '';
+		displayValueBeforeEnterNewDigit = display.value;
+		setOperand(event);
+	});
 });
+
+Array.from(operatorTypeElements).forEach((element) => {
+	element.addEventListener('click', (event) => {
+		displayValueBeforeEnterNewDigit = display.value;
+		setOperator(event);
+	});
+});
+
+display.addEventListener('keydown', (event) => {
+	if(event.key == "Backspace"){
+		removeElementFromDisplay(event);
+	}else{
+		if(display.value == 0 && !nonNumsOrOperators.test(event.key)) display.value = '';
+		
+		displayValueBeforeEnterNewDigit = display.value;
+	}
+});
+
+equalButton.addEventListener('click', calculateResult);
+
+cleanButton.addEventListener('click', removeElementFromDisplay);
